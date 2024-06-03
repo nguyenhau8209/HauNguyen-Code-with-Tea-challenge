513eee0a3cd7da846626e39cb306a70e00d35d63
Nguyễn Công Hậu <111746781+nguyenhau8209@users.noreply.github.com>
2024-06-03 09:57:36 +0000

    create project

---
+++ slide_show/src/app/page.js
@@ -0,0 +1,10 @@
+import SlideShow from "@/components/slide_show";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <SlideShow />
+    </div>
+  );
+}
---
+++ slide_show/src/components/slide_show.js
@@ -0,0 +1,7 @@
+const SlideShow = () => {
+    return (
+        <div>This is component SlideShow</div>
+    )
+};
+
+export default SlideShow;
